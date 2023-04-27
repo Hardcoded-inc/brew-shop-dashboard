b2765ca9ded89f74730425fae3fe28af577d5eff
Oskar Legner <oskarlegner@gmail.com>
2023-04-27 21:54:05 +0200

    Add useFlag base

---
+++ src/hooks/useFlag.js
@@ -0,0 +1,6 @@
+// checkFlag: (state, { value, missionId, flagId }) => {
+//   // 1. Get mission and flag
+//   // 2. Hash value with md5
+//   // 3. Check if value is correct with saved falg
+//   // 4. If correct, set "done" to true
+// },
