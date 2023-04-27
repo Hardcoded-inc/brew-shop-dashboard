cbc9a606f337fd37bae5d539d3807f9191d54cc9
Oskar Legner <oskarlegner@gmail.com>
2023-04-28 00:35:26 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 import App from "./App";
 
