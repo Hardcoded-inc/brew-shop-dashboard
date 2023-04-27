ea37c5aa3822a2414a7bee906df32cc97d1567f6
Oskar Legner <oskarlegner@gmail.com>
2023-04-28 01:22:37 +0200

    Add useTerminal to manage console state

--- src/hooks/useHistory.js
+++ src/hooks/useHistory.js
@@ -1,15 +1,20 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
+import useTerminal from "./useTerminal";
 
 const useHistory = () => {
+  const { addRecord } = useTerminal();
+
   // using Ref, because it's independent from the re-render cycle
-  const history = useRef([]);
+  // const history = useRef([]);
+  const [history, setHistory] = useState([]);
 
   // Pointer always points to the current position in the history array
   const [pointer, setPointer] = useState(-1);
 
   const addToHistory = (input) => {
-    history.current = [...history.current, input];
+    setHistory([...history, input]);
     setPointer(history.length);
+    addRecord({ value: input, type: "input" });
   };
 
   const prevInput = () => {
@@ -22,7 +27,7 @@ const useHistory = () => {
     return history[pointer];
   };
 
-  return { prevInput, nextInput, addToHistory, log: history.current };
+  return { prevInput, nextInput, addToHistory };
 };
 
 export default useHistory;
