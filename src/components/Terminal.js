485c9157f629a0168169e54b9be4d0cab67d9507
Oskar Legner <oskarlegner@gmail.com>
2023-04-27 22:47:27 +0200

    Refactor terminal history

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,14 +1,12 @@
-import { useEffect, useState, useReducer, useRef } from "react";
-import {
-  terminalReducer,
-  ACTION_TYPES,
-  initialState,
-} from "../utils/terminalReducer";
-import commandsHandler from "../utils/commandsHandler";
+import { useEffect, useState, useRef } from "react";
+
+import useHistory from "../hooks/useHistory";
+import useCommand from "../hooks/useCommand";
 
 const Terminal = () => {
   const [inputValue, setInputValue] = useState("");
-  const [state, dispatch] = useReducer(terminalReducer, initialState);
+  const { prevInput, nextInput, addToHistory } = useHistory();
+  const [applyCommand, output] = useCommand();
   const inputRef = useRef(null);
 
   useEffect(() => {
@@ -16,70 +14,20 @@ const Terminal = () => {
     terminal.scrollTop = terminal.scrollHeight;
   }, [state.output]);
 
-  const handleClickOnTerminal = () => inputRef.current.focus();
-
-  // const handleInput = (e) => {
-  //   dispatch({
-  //     type: ACTION_TYPES.SET_INPUT,
-  //     payload: e.target.value,
-  //   });
-  // };
-
-  const handleInput = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleInputHistory = (direction) => {
-    if (state.inputHistory.length === 0) return;
-
-    switch (direction) {
-      case "up":
-        if (state.historyIndex < state.inputHistory.length - 1) {
-          dispatch({
-            type: ACTION_TYPES.PREV_INPUT,
-          });
-        }
-        break;
-
-      case "down":
-        if (state.historyIndex === 0) {
-          dispatch({
-            type: ACTION_TYPES.SET_INPUT,
-            payload: "",
-          });
-        }
-
-        if (state.historyIndex > 0) {
-          dispatch({
-            type: ACTION_TYPES.NEXT_INPUT,
-          });
-        }
-        break;
-
-      default:
-        break;
-    }
-  };
-
-  const handlePost = (e) => {
-    if (inputRef.current.value === "") return;
-
-    const output = commandsHandler(e.target.value);
-
-    dispatch({
-      type: ACTION_TYPES.SET_OUTPUT,
-      payload: output,
-    });
-  };
+  const handleChange = (e) => setInputValue(e.target.value);
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") handlePost(e);
-    else if (e.key === "ArrowUp") {
-      e.preventDefault();
-      handleInputHistory("up");
-    } else if (e.key === "ArrowDown") {
-      e.preventDefault();
-      handleInputHistory("down");
+    if (e.key === "Enter") {
+      applyCommand(inputValue);
+      addToHistory(inputValue);
+    }
+    if (e.key === "ArrowUp") {
+      // e.preventDefault();
+      setInputValue(prevInput());
+    }
+    if (e.key === "ArrowDown") {
+      // e.preventDefault();
+      setInputValue(nextInput());
     }
   };
 
@@ -118,7 +66,7 @@ const Terminal = () => {
             type="text"
             value={inputValue}
             ref={inputRef}
-            onChange={handleInput}
+            onChange={handleChange}
             onKeyDown={handleKeyDown}
           />
         </div>
