58096991047bef9eeb87d3824b42e15ad2cdbdfd
Oskar Legner <oskarlegner@gmail.com>
2023-04-27 22:47:27 +0200

    Refactor terminal history

---
+++ src/hooks/useHistory.js
@@ -0,0 +1,28 @@
+import { useRef, useState } from "react";
+
+const useHistory = () => {
+  // using Ref, because it's independent from the re-render cycle
+  const history = useRef([]);
+
+  // Pointer always points to the current position in the history array
+  const [pointer, setPointer] = useState(-1);
+
+  const addToHistory = (input) => {
+    history.current = [...history.current, input];
+    setPointer(history.length);
+  };
+
+  prevInput = () => {
+    if (pointer > 0) setPointer((prev) => prev - 1);
+    return history[pointer];
+  };
+
+  nextInput = () => {
+    if (pointer < history.length - 1) setPointer((prev) => prev + 1);
+    return history[pointer];
+  };
+
+  return { prevInput, nextInput, addToHistory };
+};
+
+export default useHistory;
